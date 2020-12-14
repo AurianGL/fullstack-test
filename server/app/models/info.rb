29dc91b3cbb4b90d126675bb6475e74fa4789baf
Aurian Guerard <aurian@hotmail.fr>
2020-12-14 17:04:12 +0100

    added validation for firstname and lastname

--- server/app/models/info.rb
+++ server/app/models/info.rb
@@ -1,4 +1,6 @@
 class Info < ApplicationRecord
   has_many :messages
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   validates :email, uniqueness: true, presence: true
 end
